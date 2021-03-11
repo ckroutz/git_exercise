0ef10f18d6ea4473739208b256c9599e103f5057
candice kroutz <candice.kroutz@solidaridadnetwork.org>
2021-03-11 14:24:36 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+import PyPDF2
+import pyttsx3
+
+pdfReader = PyPDF2.PdfFileReader(open('bmgf.pdf', 'rb'))
+
+speaker = pyttsx3.init()
+rate = speaker.getProperty('rate')
+print(rate)
+speaker.setProperty('rate', 145)
+
+voices = speaker.getProperty('voices')
+print(voices)
+speaker.setProperty('voice', voices[1].id)
+
+volume = speaker.getProperty('volume')
+# print(volume)
+speaker.setProperty('volume', 1.0)
+
+# speaker.say("Hello there")
+# speaker.save_to_file("Hello WOrld!", 'test.mp3')
+
+for page_num in range(pdfReader.numPages):
+    text = pdfReader.getPage(page_num).extractText()
+    print(text)
+    speaker.say(text)
+    speaker.save_to_file(text, 'systemantics.mp3')
+    speaker.runAndWait()
+speaker.stop()
+
+
+# speaker.runAndWait()
